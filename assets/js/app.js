696df23f2f162fede39200ed548509ea0d6b73a7
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-13 12:45:20 -0500

    Interfaz

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function pestana(tab_id) {//funcion tab que recibe el parametro del id
+	var tab_contenido = document.getElementsByName("pestana");//definimos el elemento que sera devuelto
+		for(var x=0; x<tab_contenido.length; x++) {//almacenamos los elementos divs
+				if (tab_contenido[x].id == tab_id) {//comparamos el numero de contenido
+				tab_contenido[x].style.display = 'block';//mostramos el contenido correspondiente
+			}else {
+				tab_contenido[x].style.display = 'none';//ocultamos los otros contenidos.
+			}
+		}
+	}
