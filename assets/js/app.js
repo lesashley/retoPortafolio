3ba9f2e6bc72b88577510d4a77b754b2c6a91f76
cindy <mendozaic@hotmail.com>
2017-03-13 19:02:50 -0500

    agregando div lateral vertical

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ function pestana(tab_id) {//funcion tab que recibe el parametro del id
 	var tab_contenido = document.getElementsByName("pestana");//definimos el elemento que sera devuelto
 		for(var x=0; x<tab_contenido.length; x++) {//almacenamos los elementos divs
 				if (tab_contenido[x].id == tab_id) {//comparamos el numero de contenido
-				tab_contenido[x].style.display = 'block';//mostramos el contenido correspondiente
+				tab_contenido[x].style.display = 'inline-block';//mostramos el contenido correspondiente
 			}else {
 				tab_contenido[x].style.display = 'none';//ocultamos los otros contenidos.
 			}
