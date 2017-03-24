3ad43d15a2975275f5636b3813a9b6a8bae5a79d
cindy <mendozaic@hotmail.com>
2017-03-24 16:47:33 -0500

    subiendo tooltip

---
+++ Tooltip/main.js
@@ -0,0 +1,5 @@
+var link = document.getElementById('link');
+var span = document.getElementById('span');
+link.addEventListener("click",function(){
+link.style.visibility="visible";
+span.style.visibility="visible";})
