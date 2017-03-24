c476d0ab57a84bff80ebf88816fa840250e1704f
Leslie A. Lopez <lesashley56@gmail.com>
2017-03-24 16:50:20 -0500

    Agregando Local Storage

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,100 +11,121 @@ function pestana(tab_id) {//funcion tab que recibe el parametro del id
 // function popup(){
 //   window.open("popup.html","","width=300,height=150,status=no,directories=no,toolbar=no,menubar=no,scrollbars=no,location=0,resizable=no,titlebar=no");
 // }
+
+var arrayAgentes = [];
 function Agent() {
-  this.arrayAgentes = [];
-  this.id=0;
-  this.addAgentes =function(url,type,ip,ruta) {
-    this.arrayAgentes.push({
+  this.id = 0;
+  this.addAgentes =function(url,type,ip,ruta,resources) {
+    arrayAgentes.push({
       id : this.id,
       url : url,
       type : type,
       ip : ip,
       ruta : ruta,
-      resources : []
+      resources : resources
     })
     this.id ++;
   };
   this.addResource = function(indice,recurso){
-    this.arrayAgentes[indice].resources.push(recurso);
+    nuevoArray[indice].resources.push(recurso);
+		localStorage.setItem('Agentes',JSON.stringify(nuevoArray));
   };
   this.removeResource = function (indice,recurso) {
-    var rec = this.arrayAgentes[indice].resources.indexOf(recurso);
-    this.arrayAgentes[indice].resources.splice(rec,1);
+    var rec = nuevoArray[indice].resources.indexOf(recurso);
+		alert(rec);
+    nuevoArray[indice].resources.splice(rec,1);
+		localStorage.setItem('Agentes',JSON.stringify(nuevoArray));
   };
 }
 
 var agentes = new Agent();
+//Agregando en arrayAgentes
 agentes.addAgentes("bjstdmngbgr02.thoughtworks.com","Idle","192.168.1.2","/var/lib/cruise-agent",["ubuntu","firefox3","core-duo"]);
 agentes.addAgentes("bjstdmngbgr03.thoughtworks.com","Building","192.168.1.3","/var/lib/cruise-agent",["ubuntu","firefox3","mysql","core-duo"]);
 agentes.addAgentes("bjstdmngbgr04.thoughtworks.com","Building","192.168.1.4","/var/lib/cruise-agent",["ubuntu","firefox3","mysql","core-duo"]);
 agentes.addAgentes("bjstdmngbgr05.thoughtworks.com","Idle","192.168.1.5","/var/lib/cruise-agent",["ubuntu"]);
 
-//agregando recursos
-// agentes.addResource(0,"ubuntu","firefox3","core-duo");
-// agentes.addResource(1,"ubuntu,firefox3,mysql,core-duo");
-// agentes.addResource(2,"ubuntu,firefox3,mysql,core-duo");
-// agentes.addResource(3,"ubuntu");
 var idRes = 0;
 var sp = 0;
 var mostrar = document.getElementById("zlash3");
 function crearElementos() {
-  agentes.arrayAgentes.forEach(function(e) {
+    nuevoArray.forEach(function(e) {
     var panel = document.createElement('div');
     panel.setAttribute('class',e.type);
-    var figura = document.createElement('span');
-    figura.setAttribute("class","figura");
-    var circulo = document.createElement('span');
-    circulo.setAttribute("class","circulo");
-    var texto = document.createElement('span');
-    texto.setAttribute("class","texto");
-    var agente = document.createElement('span');
-    agente.innerHTML= e.url + " | " + e.type + " | " + e.ip + " | " + e.ruta;
+    var figure = document.createElement('span');
+    figure.setAttribute("class","figura");
+    var circle = document.createElement('span');
+    circle.setAttribute("class","circulo");
+    var conteiner = document.createElement('span');
+    conteiner.setAttribute("class","texto");
+    var agent = document.createElement('span');
+    agent.innerHTML= e.url + " | " + e.type + " | " + e.ip + " | " + e.ruta;
     var espacio = document.createElement("br");
     var a = document.createElement('a');
     a.setAttribute("id","sr"+sp);
     a.setAttribute("href","javascript:click("+sp+");");
     sp++;
-    a.innerHTML="Specify Resources";
-    var recursos = document.createElement('span');
-    recursos.setAttribute("id","resultado" + idRes);
+    a.innerHTML="+"+"Specify Resources";
+    var conteinerRes = document.createElement('span');
+    conteinerRes.setAttribute("id","resultado" + idRes);
     idRes++;
     mostrar.appendChild(panel);
-    panel.appendChild(figura);
-    panel.appendChild(texto);
-    figura.appendChild(circulo);
-    texto.appendChild(agente);
-    texto.appendChild(espacio);
-    texto.appendChild(a);
-    texto.appendChild(recursos);
+    panel.appendChild(figure);
+    panel.appendChild(conteiner);
+    figure.appendChild(circle);
+    conteiner.appendChild(agent);
+    conteiner.appendChild(espacio);
+    conteiner.appendChild(a);
+    conteiner.appendChild(conteinerRes);
+		var numAgent = e.id;
+		e.resources.forEach(function(i) {
+			var resource = document.createElement('span');
+			resource.setAttribute('data-id',"contRecurso");
+			resource.setAttribute("class","contRecurso");
+			var span = document.createElement('span');
+			span.setAttribute("id","sp");
+			span.innerHTML= i;
+			var deleteResource = document.createElement('button');
+			deleteResource.setAttribute("class","botones");
+			deleteResource.setAttribute("id","b");
+			deleteResource.innerHTML = "x";
+			deleteResource.addEventListener("click",function(e) {
+				var postParent = e.target.parentNode;
+				postParent.removeChild(span);
+				postParent.removeChild(deleteResource);
+				postParent.parentNode.removeChild(postParent);//elimina el nodo padre
+				agentes.removeResource(numAgent,i);
+	 		})
+	 		conteinerRes.appendChild(resource);
+	 	  resource.appendChild(span);
+	 	  resource.appendChild(deleteResource);
+		});
   })
 }
 crearElementos();
 
 function crearContenido(contenido,id) {
-  var texto = document.createElement('span');
-  texto.setAttribute('data-id',"contRecurso");
-	texto.setAttribute("class","contRecurso");
+  var resource = document.createElement('span');
+  resource.setAttribute('data-id',"contRecurso");
+	resource.setAttribute("class","contRecurso");
   var span = document.createElement('span');
   span.setAttribute("id","sp");
   span.innerHTML=contenido;
-  var eliminar = document.createElement('button');
-  eliminar.setAttribute("class","botones");
-  eliminar.setAttribute("id","b"+id);
-  eliminar.innerHTML = "x";
-  eliminar.addEventListener("click",function(e) {
+  var deleteResource = document.createElement('button');
+  deleteResource.setAttribute("class","botones");
+  deleteResource.setAttribute("id","b"+id);
+  deleteResource.innerHTML = "x";
+  deleteResource.addEventListener("click",function(e) {
     var postParent = e.target.parentNode;
     postParent.removeChild(span);
-    postParent.removeChild(eliminar);
+    postParent.removeChild(deleteResource);
     postParent.parentNode.removeChild(postParent);//elimina el nodo padre
     agentes.removeResource(id,contenido);
   })
-  texto.appendChild(span);
-  texto.appendChild(eliminar);
-  return texto;
+  resource.appendChild(span);
+  resource.appendChild(deleteResource);
+  return recurso;
 }
-
-
 
 function click(id){
    var resultado = document.getElementById("resultado" + id);
@@ -119,8 +140,8 @@ function click(id){
      });
    }
 }
-
+//Muestra en sumary
 var building = document.getElementById("building");
 var idle = document.getElementById("idle");
-building.innerHTML=document.getElementsByClassName("n1").length;
-idle.innerHTML=document.getElementsByClassName("n2").length;
+building.innerHTML=document.getElementsByClassName("Building").length;
+idle.innerHTML=document.getElementsByClassName("Idle").length;
