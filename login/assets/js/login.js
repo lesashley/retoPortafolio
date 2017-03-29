c0bb199989cf61edcc33b7b51c0741095b97e532
lesashley <lesashley56@gmail.com>
2017-03-28 22:31:16 -0500

    Login terminado

--- login/assets/js/login.js
+++ login/assets/js/login.js
@@ -3,15 +3,24 @@ var messageErrorLogin = document.getElementById('errorMessage');
 
 document.getElementById('login').addEventListener("click",function(){
   if(inputsLogin[0].value == "" ){
-    messageErrorLogin.innerHTML = "Don't accept empty fields";}
-    else if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(inputsLogin[0].value) != true){
-      messageErrorLogin.innerHTML="Enter valid email";}
-      else if(inputsLogin[1].value == "" ){
-        messageErrorLogin.innerHTML = "Don't accept empty fields";}
-        else if(inputsLogin[1].value.length <= 6 || inputsLogin[1].value == "password" || inputsLogin[1].value == "123456") {
-          messageErrorLogin.innerHTML="Use alphanumerical, more than six characters";}
-          else if(inputsLogin[0].value == "mendozaic@hotmail.com" && inputsLogin[1].value == "pruebalogin" || inputsLogin[0].value == "leslie@gmail.com" && inputsLogin[1].value == "pruebaLogin1"){
-            window.location="../master/index.html";
-          }else{ messageErrorLogin.innerHTML="Wrong email and password";}
-
+    messageErrorLogin.innerHTML = "Don't accept empty fields";
+  }
+  else if (/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(inputsLogin[0].value) != true){
+    messageErrorLogin.innerHTML="Enter valid email";
+  }
+  else if(inputsLogin[1].value == "" ){
+    messageErrorLogin.innerHTML = "Don't accept empty fields";
+  }
+  else if(inputsLogin[1].value == "password" || inputsLogin[1].value.length < 6) {
+    messageErrorLogin.innerHTML="Use more than five characters";
+  }
+  else if(inputsLogin[0].value != "" && inputsLogin[1].value != ""){
+    usersCruise.forEach(function (e) {
+      if (inputsLogin[0].value == e.email && inputsLogin[1].value == e.password) {
+        window.location="../index.html";
+      } else{
+        messageErrorLogin.innerHTML="Wrong email and password";
+      }
+    });
+  }
 });
